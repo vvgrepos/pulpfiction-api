abce24920c34b75bf5226897259bca502108cd2f
Vinicius Barbosa <vini.sb2808@gmail.com>
2018-09-01 13:47:27 -0300

    Conexão com Postgree

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { Client } = require('pg');
+const client = new Client();
+
 router.get('/', function (req, res, next) {
     res.status(200).send({
         title: "Barbosinha é gay!",
-        version: "0.0.1"
+        version: "0.0.1",
+        rodigao: "viado",
+        gavetinha: "humilde",
+        barbosinha: "rico",
+        adiel: "igreja"
     });
+
+    getUser();
 });
+
+function getUser(){
+    await client.connect()
+    let res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message)
+    await client.end()
+}
+
 module.exports = router;
